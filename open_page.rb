63b40b8ef0264a098ab83741fdcd042d0fbb9f7c
Daniel Choi <dhchoi@gmail.com>
2012-05-01 14:21:37 -0400

    open_page.rb script lets you open doc from command line, using elinks

--- open_page.rb
+++ open_page.rb
@@ -5,10 +5,34 @@ require 'open-uri'
 term = ARGV[0] || 'UISplitViewController'
 base = '/Users/choi/Library/Developer/Shared/Documentation/DocSets/com.apple.adc.documentation.AppleiOS5_1.iOSLibrary.docset/Contents/Resources/Documents/documentation'
 
-matches = open("|find '#{base}' -name 'index.html' -ipath '*#{term}*'").readlines
+matches = open("|cd #{base} && find . -name 'index.html' -ipath '*#{term}*'").readlines
 
 puts matches
 
-exec "open #{matches[0]}"
+#exec "open #{matches[0]}"
+
+if matches.empty?
+  puts "no matches"
+  exit
+end
+
+target = matches[0]
+
+#exec "cd #{base} && elinks #{target}"
+
+output = `cd #{base} && elinks --dump #{target}`
+
+if (redirect_target= output[/Refresh: \[1\](.*)$/, 1])
+  puts target
+  target = target.sub(%r!/[^/]*$!, '/' + redirect_target)
+  puts "Redirecting to: #{target}"
+  output = `cd #{base} && elinks --dump-width 120 --dump #{target}`
+
+end
+puts output
 
 
+__END__
+
+./Cocoa/Reference/Foundation/Classes/NSArray_Class/index.html
+   Refresh: [1]NSArray.html
